5d058dcfa8d54c76cff7bd51d5f28a3d6a59addf
Frank ZHou <zhiyan.zhou@stonybrookschool.org>
2018-11-06 13:31:28 -0500

    minor changes

--- processTargetPhoto.py
+++ processTargetPhoto.py
@@ -11,15 +11,16 @@ def process_whole(img):
     img = cv2.resize(img, (TARGET_IMAGE_SIZE, TARGET_IMAGE_SIZE))
     return img
 
-def apply_filter(img, tiles)
+
+def apply_filter(img, tiles):
     for row in range(0, TARGET_IMAGE_SIZE, FILTER_SIZE):
         for col in range(0, TARGET_IMAGE_SIZE, FILTER_SIZE):
             curr_input = img[row:row+20][col:col+20]
             clo_dis = 0
             clo_img = None
-            np.random.shu
+            np.random.shuffle(tiles)
             for tile in tiles:
                 curr_dis = color_distance(curr_input, tile)
-                if clo_img == None or curr_dis < clo_dis:
+                if clo_img is None or curr_dis < clo_dis:
                     clo_dis = curr_dis
-                    img[row:row + 20][col:col + 20] = tiles
+            img[row:row + 20][col:col + 20] = tiles
